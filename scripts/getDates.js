1d30aab23ab126b3c7c98dac54be375d4f3c80bd
johnbrightagbons <brightinojohnny@gmail.com>
2023-09-22 01:36:35 +0100

    Dark mode

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -21,3 +21,18 @@ document.addEventListener("DOMContentLoaded", function () {
     lastModifiedDateElement.textContent = formattedLastModifiedDate;
   }
 });
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("☑️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "❎";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "☑️";
+	}
+});
