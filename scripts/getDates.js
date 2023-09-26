f200fcb8ef18e275a957aaa66d220c40eae14f56
johnbrightagbons <brightinojohnny@gmail.com>
2023-09-26 01:36:25 +0100

    change

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,12 +1,10 @@
 // Get the current year
 const currentYear = new Date().getFullYear();
+document.querySelector('#year').textContent = currentYear; // Set the current year in the 'year' element
 
 // Get the last modified date from the document
 const lastModifiedDate = new Date(document.lastModified);
-
-// Format the date as "Month Day, Year" (e.g., "September 13, 2023")
-alert(document.lastModified); {
-};
+document.querySelector('#lastModified').textContent = "Last Modified: " + lastModifiedDate.toLocaleString(); // Set the formatted last modified date in the 'lastModified' element
 
 // Update the HTML content with the current year and last modified date
 document.addEventListener("DOMContentLoaded", function () {
@@ -14,24 +12,26 @@ document.addEventListener("DOMContentLoaded", function () {
   const lastModifiedDateElement = document.querySelector("#last-modified-date");
 
   if (copyrightYearElement) {
-    copyrightYearElement.textContent = currentYear;
+    copyrightYearElement.textContent = currentYear; // Set the current year in the 'current-year' element
   }
 
   if (lastModifiedDateElement) {
-    lastModifiedDateElement.textContent = formattedLastModifiedDate;
+    lastModifiedDateElement.textContent = lastModifiedDate.toLocaleString(); // Set the formatted last modified date in the 'last-modified-date' element
   }
 });
 
+// Dark mode toggle
+const main = document.querySelector("main");
 const modeButton = document.querySelector("#mode");
 
 modeButton.addEventListener("click", () => {
-	if (modeButton.textContent.includes("☑️")) {
-		main.style.background = "#000";
-		main.style.color = "#fff";
-		modeButton.textContent = "❎";
-	} else {
-		main.style.background = "#eee";
-		main.style.color = "#000";
-		modeButton.textContent = "☑️";
-	}
+  if (main.classList.contains("dark-mode")) {
+    // If dark mode is currently active, switch to light mode
+    main.classList.remove("dark-mode");
+    modeButton.textContent = "☑️"; // Update the button text
+  } else {
+    // If light mode is currently active, switch to dark mode
+    main.classList.add("dark-mode");
+    modeButton.textContent = "❎"; // Update the button text
+  }
 });
